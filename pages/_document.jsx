05e067e8cbb37d2b0ab9b6d93d11a1a9e53146e7
Kasper Aamodt <kasper@aamodt.xyz>
2022-01-06 19:13:45 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { extractCss } from 'goober'
 
 export default class MyDocument extends Document {
+    static async getInitialProps({ renderPage }) {
+        const page = await renderPage()
+        // Extrach the css for each page render
+        const css = extractCss()
+        return { ...page, css }
+    }
     render() {
         return (
             <Html lang="en">
                 <Head>
                     <link rel="icon" href="/favicon.ico" />
+                    <style
+                        id={'_goober'}
+                        dangerouslySetInnerHTML={{ __html: ' ' + this.props.css }}
+                    />
                 </Head>
                 <body>
                     <Main />
