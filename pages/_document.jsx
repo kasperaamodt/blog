7d0cbe43ee97d09dc5f5d4e0a5e0dbbc2a71b3e4
Kasper Aamodt <kasper@aamodt.xyz>
2022-01-06 21:40:41 +0100

    new icon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="icon" href="/images/icon.png" />
                     <style
                         id={"_goober"}
                         dangerouslySetInnerHTML={{
