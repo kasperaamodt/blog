c3b0eb4edee162142ce8b45cc7649eb153c73af6
Kasper Aamodt <kasper@aamodt.xyz>
2022-02-05 21:17:45 +0100

    Update analytics.js

--- pages/api/analytics.js
+++ pages/api/analytics.js
@@ -14,10 +14,5 @@ export default async function handler(req, res) {
         return res.status(500).json({ error: "Error retrieving data" });
     }
 
-    res.setHeader(
-        "Cache-Control",
-        "public, s-maxage=1200, stale-while-revalidate=600"
-    );
-
     return res.status(200).json(data);
 }
