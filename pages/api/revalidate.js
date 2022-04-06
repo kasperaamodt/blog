403e4d4ae1d848588269648d12771b2a9ed609b4
Kasper Aamodt <kasper@aamodt.xyz>
2022-04-06 14:07:07 +0200

    Update revalidate.js

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     if (req.query.secret !== process.env.REVALIDATE) {
-        return res.status(401).json({ message: "Invalid token" });
+        return res.status(401).json({ message: "Invalid token, " + process.env.REVALIDATE + " . " + req.query.secret });
     }
 
     try {
