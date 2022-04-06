c728523571f8bb889271c5d2390bc74c55ebcee1
Kasper Aamodt <kasper@aamodt.xyz>
2022-04-06 13:57:33 +0200

    Update revalidate.js

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,9 +3,18 @@ export default async function handler(req, res) {
         return res.status(401).json({ message: "Invalid token" });
     }
 
+    const slug = undefined;
+
+    if (req.query.slug) {
+        slug = req.queryl.slug
+    }
+
+    console.log(req);
+
     try {
         await res.unstable_revalidate("/");
         await res.unstable_revalidate("/blog");
+        await res.unstable_revalidate(`/blog/${slug}`);
         return res.json({ revalidated: true });
     } catch (err) {
         return res.status(500).send("Error revalidating");
