2116252a56f7c879bcc26e7cd91e51c86fff5c5b
Kasper Aamodt <kasper@aamodt.xyz>
2022-04-06 12:00:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,5 @@ module.exports = {
     reactStrictMode: true,
     images: {
         domains: ["wp.aamodt.xyz", "wp.aamodtgroup.com", "localhost"]
-    },
-    webpack: (config, { dev, isServer }) => {
-        // Replace React with Preact only in client production build
-        if (!dev && !isServer) {
-            Object.assign(config.resolve.alias, {
-                react: "preact/compat",
-                "react-dom/test-utils": "preact/test-utils",
-                "react-dom": "preact/compat"
-            });
-        }
-        return config;
     }
 };
