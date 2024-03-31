c41f0fa5b9a83b264b01648b0a70426a325311df
Kasper Aamodt <kasper@aamodt.xyz>
2024-03-31 05:38:28 +0200

    astro v4, new images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
-import image from '@astrojs/image';
-
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx()]
+	integrations: [mdx()],
 });
